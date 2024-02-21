51fd2c07977c296c5be461a7b6190227e858aae0
013Meta <rudydube@protonmail.com>
2024-02-21 22:50:52 +0100

    with st_rec

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,15 +1,15 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
-from dotenv import load_dotenv
+from typing import Union
 
-load_dotenv()
+from fastapi import FastAPI
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
 
-@app.get("/", response_class=HTMLResponse)
-def get(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
